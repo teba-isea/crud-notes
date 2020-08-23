e10d9d88acea9ac39fd800dd5b607995e1ee9f0f
Esteban Isea <iseaesteban@gmail.com>
2020-08-23 06:31:05 +0000

    connectiong to a external db to test online

--- database.js
+++ database.js
@@ -5,11 +5,13 @@ const DB_PASSWORD = "your_password";
 const DB_HOSTNAME = "127.0.0.1";
 const DB_PORT = "27017";
 const DB_NAME = "crud-db";
-const url  = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOSTNAME}:${DB_PORT}/${MONGO_DB}?authSource=admin`;
+const url  = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOSTNAME}:${DB_PORT}/${MONGO_DB}?authSource=admin`;
 const localUrl ="mongodb://localhost/crud-db";
+// my test dbs
+const testDb = mongodb+srv://teba-isea:bLeaJmG1dFeHp2Vj@cluster0.jm87x.mongodb.net/crud-db?retryWrites=true&w=majority
 
 database
-  .connect(localUrl, { useNewUrlParser: true })
+  .connect(testDb, { useNewUrlParser: true })
   .then((db) => console.log(`Database already: ${localUrl}`))
   .catch((err) => console.error(`Can't connect to the database: ${err}`));
 
